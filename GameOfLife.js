65ddffe1aaf1aec2ff37856b7bbfa6c2d92fa3a3
Jordan Raleigh <jraleigh1127@gmail.com>
2020-04-24 12:06:34 -0500

    first commit

--- GameOfLife.js
+++ GameOfLife.js
@@ -10,15 +10,16 @@ class GameOfLife {
    */
 
   makeBoard() {
-    // TODO: Create and return an 2D Array 
-    // with `this.heigh` as rows and `this.width` as cols.
-    // For example, given a height of 4 and a width of 3, it will generate:
-    // [
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    // ]
+    let board = []
+    for(let i = 0; i < this.height; i++){
+      let row = []
+        for(let j = 0; j < this.width; j++){
+          row.push(0)
+      }
+      board.push(row)
+    }
+    console.log(board)
+    return board
   }
 
 
@@ -28,8 +29,39 @@ class GameOfLife {
 
   livingNeighbors(row, col) {
     // TODO: Return the count of living neighbors.
+    //given cells position, return number of living neighbors 
+    //use utility methods to get neighbors 
+    // get cell then go to living neighbors
+    let count = 0
+    if(this.getCell(row-1, col) === 'alive') count++
+    if(this.getCell(row-1, col+1) === 'alive') count++
+    if(this.getCell(row-1, col-1) === 'alive') count++
+    if(this.getCell(row, col-1) === 'alive') count++
+    if(this.getCell(row, col+1) === 'alive') count++
+    if(this.getCell(row+1, col) === 'alive') count++
+    if(this.getCell(row+1, col-1) === 'alive') count++
+    if(this.getCell(row+1, col+1) === 'alive') count++
+    return count
+  }
+  getCell(row, col){
+    if(row < 0 || row > this.height || col < 0 || col > this.width){
+      return 'dead'
+    }
+    let check = this.board[row][col]
+    if(check === 1){
+      return 'alive'
+    } else {
+      return 'dead'
+    }
   }
 
+  toggleCell(row, col){
+    if(this.board[row][col] === 1){
+      this.board[row][col] = 0
+    } else {
+      this.board[row][col] = 1 
+    }
+  }
 
   /**
    * Given the present board, apply the rules to generate a new board
@@ -44,8 +76,20 @@ class GameOfLife {
     //
     // You need to:
     // 1. Count alive neighbors for all cells
+    for(let i = 0; i < this.board.length; i++){
+      for(let j = 0; j < this.board[i].length; j++){
+        let check = this.livingNeighbors(i,j)
+        // if(check === 0){
+          debugger
+          console.log("works") 
+        // }
+      }
+    }
     // 2. Set the next state of all cells in newBoard,
     // based on their current alive neighbors
     this.board = newBoard;
   }
 }
+
+
+// grid[],[]
